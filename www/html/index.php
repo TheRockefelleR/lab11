3403b5dc8b95376a3f3fddcbff3c449abed04bb0
TheRockefelleR <84729968+TheRockefelleR@users.noreply.github.com>
2023-05-31 23:09:44 +0200

    Add files via upload

---
+++ www/html/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>  
+     <head>  
+      <title>app</title>
+     </head>  
+
+     <body>  
+      <h1>Konrad Skolimowski</h1>
+      <p><?php echo 'PHP, version: ' . phpversion(); ?></p>
+     </body>
